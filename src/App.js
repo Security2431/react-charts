4aecb7be1fa7d6c46021358b99163a7074c51913
Security2431 <Artem2431@gmail.com>
2018-05-23 20:28:52 +0300

    add new pamm accounts: Sunnich M v2, Method_from_Bakir, Miracletrading new

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import ChartList from './ChartList'
 import './style.css';
 
-const accounts = ['374756', '408559', '376862', '219161', '381174', '198572', '381546', '386780', '329842']
-const labels =   ['ST PRIORITY', 'Constellation', 'Night Owl Turbo', 'Hercules', 'SunTime', 'Expensivebuyer', 'Rumpelstilzchen Rage', 'RoboCop 2020 x 2', 'Moriarti']
+const accounts = ['374756', '408559', '376862', '219161', '381174', '198572', '381546', '386780', '329842', '360381', '369586', '385128']
+const labels =   ['ST PRIORITY', 'Constellation', 'Night Owl Turbo', 'Hercules', 'SunTime', 'Expensivebuyer', 'Rumpelstilzchen Rage', 'RoboCop 2020 x 2', 'Moriarti', 'Miracletrading new', 'Sunnich M v2', 'Method_from_Bakir' ]
 
 class App extends Component {
   render() {
